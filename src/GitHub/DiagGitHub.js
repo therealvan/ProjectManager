b9aca5976139c5e894045b4b19a341766a91c51a
Shadow <shadow@example.com>
2025-03-12 14:31:55 +0100

    Initialisation du projet depuis StartProject.js

--- src/GitHub/DiagGitHub.js
+++ src/GitHub/DiagGitHub.js
@@ -1,5 +1,5 @@
 // DiagGitHub.js - Module de débogage conditionnel
 function log(message) {
-    console.error('Erreur détectée : ' + message);
+    console.error("Erreur détectée : " + message);
 }
 module.exports = { log };
