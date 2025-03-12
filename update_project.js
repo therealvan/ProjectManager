b24544253d4cdca0e9daae21fc6886a272e3c967
Shadow <shadow@example.com>
2025-03-12 16:25:46 +0100

    Initialisation du projet depuis StartProject.js

---
+++ update_project.js
@@ -0,0 +1,6 @@
+// update_project.js (Version pour lancer StartProject.js uniquement)
+const { startProject } = require('./StartProject.js');
+
+startProject();
+
+module.exports = { startProject };
