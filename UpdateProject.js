bcdbc0a44b74a662219f93aaee04acdaed5503c8
Shadow <shadow@example.com>
2025-03-15 16:05:51 +0100

    Automatic project update

--- UpdateProject.js
+++ UpdateProject.js
@@ -1,24 +1,15 @@
-// UpdateProject.js
+// UpdateProject.js - Push local changes to GitHub repository
 const fs = require('fs');
 const path = require('path');
-const { pushChanges, deleteBranch } = require('./src/GitHub/GitHub.js');
 
 // Redirect console output to project.log
 const logStream = fs.createWriteStream(path.join(__dirname, 'project.log'), { flags: 'a' });
 console.log = (...args) => logStream.write(args.join(' ') + '\n');
 
-// Push local changes to the current branch
+// Import GitHub functions
+const { pushChanges } = require('./src/GitHub/GitHub.js');
+
+// Push local changes to the repository
 pushChanges();
 
-// Delete the remote branches 'test' and 'test2'
-deleteBranch('test');
-deleteBranch('test2');
-
-// Project tree structure for this update
-/*
-├── src/
-│   └── GitHub/
-│       └── GitHub.js  # Used for push and branch deletion
-├── UpdateProject.js   # This file
-└── project.log        # Log file for tracing actions
-*/
+logStream.end();
