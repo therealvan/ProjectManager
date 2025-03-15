e0b782c9acca965aa065f7af411b32da21af6e1e
Shadow <shadow@example.com>
2025-03-15 12:46:18 +0100

    Automatic project update

--- UpdateProject.js
+++ UpdateProject.js
@@ -3,7 +3,7 @@ const { pushChanges } = require('./src/GitHub/GitHub.js');
 function updateProject() {
     console.log('Starting UpdateProject.js...');
     pushChanges();
-    console.log('Local project successfully pushed to the repository.');
+    console.log('Local code successfully pushed to the repository.');
 }
 
 updateProject();
