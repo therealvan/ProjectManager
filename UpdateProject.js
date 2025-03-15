69f3bdee8e3b3f875ffdd82d4a989158a117751e
Shadow <shadow@example.com>
2025-03-15 14:18:17 +0100

    Automatic project update

--- UpdateProject.js
+++ UpdateProject.js
@@ -1,9 +1,17 @@
 // UpdateProject.js
-const { pushChanges } = require('./src/GitHub/GitHub.js');
+const fs = require('fs');
+const path = require('path');
+const github = require(path.join(__dirname, 'src', 'GitHub', 'GitHub.js'));
 
 function updateProject() {
-    // Push local changes to the remote repository
-    pushChanges();
+    // Create a new branch named "test"
+    github.createBranch('test');
+    
+    // Push local changes to the "test" branch
+    github.pushChanges();
 }
 
+// Execute the update process
 updateProject();
+
+module.exports = { updateProject };
