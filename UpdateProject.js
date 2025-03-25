c7a9db536a9dbc2bd876a152eb530833b92f7ddf
Shadow <shadow@example.com>
2025-03-25 19:45:29 +0100

    Mise à jour du projet depuis local

--- UpdateProject.js
+++ UpdateProject.js
@@ -1,11 +1,11 @@
 const { addFiles, commitChanges, pushChanges } = require('./src/GitHub/GitHub.js');
 
 function updateProject() {
-    // Add all local files to staging
+    // Add all local files to the staging area
     addFiles('.');
 
-    // Commit changes with a message
-    commitChanges('Mise à jour locale vers le dépôt GitHub');
+    // Commit the changes with a message
+    commitChanges('Mise à jour du projet depuis local');
 
     // Push changes to the remote repository
     pushChanges();
